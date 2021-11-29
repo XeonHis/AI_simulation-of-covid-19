6910fa24f8052696faf81b3bba5abbd66cd06d67
xeonhis_mac <yingheting@gmail.com>
2021-11-29 19:21:25 +0000

    test model function is integrated into dqn

--- test_model.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-test = [[0.09543565, 0.07267367], [0.07391742, 0.07343835], [0.05148294, 0.0503968], [0.08201064, 0.0206894]]
-print(np.amax(test, axis=0))
-print(np.max(test, axis=0))
